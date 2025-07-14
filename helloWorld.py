cdb3941a58a8928b29c860a2e81259f977fb9a19
bella-tsh <thesu.h607@gmail.com>
2025-07-14 07:00:57 +0000

    save 	new file:   Bootcamp exercise 1 	new file:   Ex 2

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,6 @@
-
+longestName = ""
+for i in range (10):
+    name = input("Enter name: ")
+    if (len(name)) > len(longestName):
+        longestName = name 
+print (longestName + "is the longest name")
